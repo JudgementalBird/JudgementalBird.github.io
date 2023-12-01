7b5b725b6c4ec639e303769f1aa94a248cdf1875
JudgementalBird <judgementalbird@gmail.com>
2023-12-01 20:24:01 +0100

    jeppers peppers

---
+++ code.js
@@ -0,0 +1,180 @@
+var Lego = {
+    "F-14": [{
+        "link": "https://www.amazon.com/COBI-TOP-Gun-F-14-Tomcat/dp/B08YRTLC49?ref_=ast_sto_dp", 
+        "host": "Amazon", 
+        "name": "F-14A Jagerfly"
+    }],
+
+    "F-16": [{
+        "link": "https://www.amazon.com/DAHONPA-F-16-Fighting-521Pieces-Air-Force/dp/B09XM7L4V8/ref=sr_1_28?keywords=fighter+jet+lego&qid=1701433191&sr=8-28", 
+        "host": "Amazon", 
+        "name": "F-16 Jagerfly"
+    },
+        {"link": "https://www.amazon.com/dp/B0CL69N744/ref=sr_1_46?crid=GTV959F9MZED&keywords=F16&qid=1701447704&sprefix=f15%2Caps%2C679&sr=8-46", 
+        "host": "Amazon", 
+        "name": "F-16 Jagerfly"
+    }],
+
+    "F-18": [{
+        "link": "https://www.amazon.com/Planes-Airplane-Fighter-Military-Building/dp/B0C38XKYQN/ref=sr_1_34?keywords=fighter+jet+lego&qid=1701433191&sr=8-34", 
+        "host": "Amazon", 
+        "name": "F/A-18E Jagerfly"
+    },
+        {"link": "https://www.amazon.com/Military-Airplane-Compatible-Construction-Enthusiasts/dp/B0CB471W22/ref=sr_1_10?keywords=fighter%2Bjet%2Blego&qid=1701433191&sr=8-10&th=1", 
+        "host": "Amazon", 
+        "name": "F/A-18E Jagerfly"
+    },
+        {"link": "https://www.amazon.com/DAHONPA-Bumblebee-Military-Airplane-Air-Force/dp/B09N8RQRJJ/ref=sr_1_31?keywords=fighter+jet+lego&qid=1701433191&sr=8-31", 
+        "host": "Amazon", 
+        "name": "F/A-18E Jagerfly"
+    }],
+
+    "F-15": [{
+        "link": "https://www.amazon.com/dOvOb-Military-Fighter-Building-Blocks/dp/B0B8Z7T5B7/ref=sr_1_29?keywords=fighter+jet+lego&qid=1701433191&sr=8-29", 
+        "host": "Amazon", 
+        "name": "F-15 Jagerfly"
+    }],
+
+    "F-117": [{
+        "link": "https://www.amazon.com/Ultimate-Soldier-Stealth-Military-Building/dp/B00MXE7TZ0/ref=sr_1_37?keywords=fighter+jet+lego&qid=1701433191&sr=8-37", 
+        "host": "Amazon", 
+        "name": "F-117 Jagerfly"
+    }],
+
+    "F-35": [{
+        "link": "https://www.amazon.com/XINGBAO-Military-Lightning-Interlock-Educational/dp/B07X8TZGNY/ref=sr_1_23?keywords=fighter+jet+lego&qid=1701433191&sr=8-23", 
+        "host": "Amazon", 
+        "name": "F-35b? Jagerfly"
+    }],
+
+    "SR-71": [{
+        "link": "https://www.fruugonorge.com/blackbird-rekognoseringsfly-us-air-force-building-blocks-plug-in-jagerfly-swat-minifigur-leketoy-kompatibel-med-lego-f/p-131399256-276436728", 
+        "host": "Fruugonorge", 
+        "name": "SR-71 Rekogniseringsfly"
+    },
+        {"link": "https://www.amazon.com/SEMKY-Military-Reconnaissance-Blackbird-Building/dp/B09Q8ZC33L/ref=sr_1_30?keywords=fighter+jet+lego&qid=1701433191&sr=8-30", 
+        "host": "Amazon", 
+        "name": "SR-71 Rekogniseringsfly"
+    },
+        {"link": "https://www.amazon.com/DAHONPA-Blackbird-High-Altitude-Reconnaissance-Air-Force/dp/B09XQNP2XK/ref=sr_1_16?crid=2NJJCJFEFHG80&keywords=Sr71&qid=1701451526&sprefix=sr%2Caps%2C236&sr=8-16", 
+        "host": "Amazon", 
+        "name": "SR-71 Rekogniseringsfly"
+    },
+        {"link": "https://www.amazon.com/Artorbricks-Reconnaissance-Blackbird-Military-Compatible/dp/B0CKWJSDKV/ref=sr_1_18?crid=2NJJCJFEFHG80&keywords=Sr71&qid=1701451526&sprefix=sr%2Caps%2C236&sr=8-18", 
+        "host": "Amazon", 
+        "name": "SR-71 Rekogniseringsfly"
+    }]
+};
+
+var Modeller = {
+    "F-35": [{
+        "link": "https://www.amazon.com/72-F-35-Lightning-Ii-Version/dp/B0792X1Z92/ref=sr_1_7?crid=3K2B5NG6ELFO8&keywords=F35&qid=1701447381&sprefix=f3%2Caps%2C197&sr=8-7", 
+        "host": "Amazon", 
+        "name": "F-35B Jagerfly"
+    },
+    {
+        "link": "https://www.amazon.com/F-35A-Lightning-Aircraft-Military-Collection/dp/B0CH81HHYM/ref=sr_1_30?crid=3K2B5NG6ELFO8&keywords=F35&qid=1701447381&sprefix=f3%2Caps%2C197&sr=8-30", 
+        "host": "Amazon", 
+        "name": "F-35A Jagerfly"
+    }],
+    "F-18": [{
+        "link": "https://www.amazon.com/04994-32-18E-Super-Hornet/dp/B01D4QFBFA/ref=sr_1_27?crid=29P9US3O7L3JA&keywords=f18+model&qid=1701448443&sprefix=f18+mo%2Caps%2C216&sr=8-27        ", 
+        "host": "Amazon", 
+        "name": "F/A-18E Jagerfly"
+    }],
+    "F-16": [{
+        "link": "https://www.amazon.com/Tamiya-America-Lockheed-F-16CJ-TAM61098/dp/B000MTIF1K/ref=sr_1_18?crid=GTV959F9MZED&keywords=F16&qid=1701447704&sprefix=f15%2Caps%2C679&sr=8-18        ", 
+        "host": "Amazon", 
+        "name": "F-16 Jagerfly"
+    }],
+    "F-14": [{
+        "link": "https://www.amazon.com/Tamiya-America-Grumman-Tomcat-TAM61114/dp/B01KZA225A/ref=sr_1_8?crid=2F4OH0XDVEGC&keywords=F-14&qid=1701447824&sprefix=f-14%2Caps%2C208&sr=8-8        ", 
+        "host": "Amazon", 
+        "name": "F-14A Jagerfly"
+    }],
+    "F-22": [{
+        "link": "https://www.amazon.com/Hasegawa-48-F-22-Raptor-USAF/dp/B0038JCAKI/ref=sr_1_16_mod_primary_new?crid=31VWCYPJO6ZF3&keywords=F22&qid=1701447589&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=f%2Caps%2C421&sr=8-16        ", 
+        "host": "Amazon", 
+        "name": "F-22 Jagerfly"
+    }],
+    "Su-35": [{
+        "link": "https://www.amazon.com/HANGHANG-Attack-Plane-Commemorate-Collection/dp/B09XTM6GBW/ref=sr_1_8?crid=3SV3PYQ84RDCB&keywords=su35&qid=1701448178&sprefix=su35%2Caps%2C247&sr=8-8&th=1", 
+        "host": "Amazon", 
+        "name": "Su-35 Jagerfly"
+    },
+    {
+        "link": "https://www.amazon.com/Lose-Fun-Park-Fairchild-Airplanes/dp/B08GWT4LC4/ref=sr_1_10?crid=3SV3PYQ84RDCB&keywords=su35&qid=1701448178&sprefix=su35%2Caps%2C247&sr=8-10&th=1", 
+        "host": "Amazon", 
+        "name": "Su-35 Jagerfly"
+    },
+    {
+        "link": "https://www.amazon.com/RiToEasysports-35-Simulation-Military-Collection/dp/B0BGMXVS7D/ref=sr_1_15?crid=3SV3PYQ84RDCB&keywords=su35&qid=1701448178&sprefix=su35%2Caps%2C247&sr=8-15", 
+        "host": "Amazon", 
+        "name": "Su-35 Jagerfly"
+    },
+    {
+        "link": "https://www.amazon.com/Master-Flanker-Ahktubinsk-diecast-Aircraft/dp/B07YHMG71J/ref=sr_1_24?crid=3SV3PYQ84RDCB&keywords=su35&qid=1701448178&sprefix=su35%2Caps%2C247&sr=8-24", 
+        "host": "Amazon", 
+        "name": "Su-35 Jagerfly"
+    },
+    {
+        "link": "https://www.amazon.com/Sukhoi-SU35-Aircraft-Airplane-Collection/dp/B0BLYBCNDX/ref=sr_1_28?crid=3SV3PYQ84RDCB&keywords=su35&qid=1701448178&sprefix=su35%2Caps%2C247&sr=8-28", 
+        "host": "Amazon", 
+        "name": "Su-35 Jagerfly"
+    }
+    ],
+    "B-2": [{
+        "link": "https://www.amazon.com/B-2A-Airplane-Aircraft-Collection-Ornament/dp/B0BLV9SRKP/ref=sr_1_9?crid=1YZ5D4X70992Z&keywords=B-2+bomber+model&qid=1701448499&sprefix=b-2+bomber+mo%2Caps%2C219&sr=8-9        ", 
+        "host": "Amazon", 
+        "name": "B-2 Bombefly"
+    }],
+    "SR-71": [{
+        "link": "https://www.amazon.com/Mahfisj-Scale-SR-71-Blackbird-Model/dp/B0CFYPVMRY/ref=sr_1_36?crid=2NJJCJFEFHG80&keywords=Sr71&qid=1701451526&sprefix=sr%2Caps%2C236&sr=8-36", 
+        "host": "Amazon", 
+        "name": "SR-71 Rekogniseringsfly"
+    },
+    {
+        "link": "https://www.amazon.com/Revell-04967-Lockheed-SR-71-Blackbird/dp/B08XD4ZWG3/ref=sr_1_8?crid=2NJJCJFEFHG80&keywords=Sr71&qid=1701451526&sprefix=sr%2Caps%2C236&sr=8-8", 
+        "host": "Amazon", 
+        "name": "SR-71 Rekogniseringsfly"
+    },
+    {
+        "link": "https://www.amazon.com/NUOTIE-SR-71A-Blackbird-Collectible-Collection/dp/B0BRXKKVKV/ref=sr_1_35?crid=2NJJCJFEFHG80&keywords=Sr71&qid=1701451526&sprefix=sr%2Caps%2C236&sr=8-35&th=1", 
+        "host": "Amazon", 
+        "name": "SR-71 Rekogniseringsfly"
+    }],
+    "F-15": [{
+        "link": "https://www.amazon.com/Revell-F-15C-Eagle-Plastic-Model/dp/B01N2AXFQI/ref=sr_1_18?crid=1NP296TN4DRRE&keywords=F-15&qid=1701447940&sprefix=f-15%2Caps%2C207&sr=8-18        ", 
+        "host": "Amazon", 
+        "name": "F-15C Jagerfly"
+    }],
+};
+
+// Function to create list items for each item in an object
+function createListItems(containerId, dataObject) {
+    const container = document.getElementById(containerId);
+    for (const itemName in dataObject) {
+      if (dataObject.hasOwnProperty(itemName)) {
+        const items = dataObject[itemName];
+        for (let i = 0; i < items.length; i++) {
+          const itemData = items[i];
+          const listItem = document.createElement("li");
+          const link = document.createElement("a");
+          link.href = itemData.link;
+          link.textContent = itemData.host + ": " + itemData.name + (items.length > 1 ? ` (${i + 1})` : '');
+          listItem.appendChild(link);
+          container.appendChild(listItem);
+        }
+      }
+    }
+  }
+  
+  // Call the function to create list items for Lego and Modeller
+  createListItems("lego-list", Lego);
+  createListItems("modeller-list", Modeller);
+  
+  
+  // Call the function to create list items for Lego and Modeller
+  createListItems("lego-list", Lego);
+  createListItems("modeller-list", Modeller);
+  
